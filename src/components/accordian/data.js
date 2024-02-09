f108839d7cb32968899cb51ddff367533d6251c5
Walter <2760walter@gmail.com>
2024-02-09 07:26:54 +0300

    display the questions, and then asnwers on click, single

---
+++ src/components/accordian/data.js
@@ -0,0 +1,55 @@
+const data = [
+    {
+        id: 1,
+        question: "What is the capital of France?",
+        answer: "Paris"
+    },
+    {
+        id: 2,
+        question: "What is the largest planet in our solar system?",
+        answer: "Jupiter"
+    },
+    {
+        id: 3,
+        question: "Who wrote 'Romeo and Juliet'?",
+        answer: "William Shakespeare"
+    },
+    {
+        id: 4,
+        question: "What is the chemical symbol for water?",
+        answer: "H2O"
+    },
+    {
+        id: 5,
+        question: "What is the tallest mammal on Earth?",
+        answer: "Giraffe"
+    },
+    {
+        id: 6,
+        question: "What is the currency of Japan?",
+        answer: "Yen"
+    },
+    {
+        id: 7,
+        question: "What is the chemical symbol for gold?",
+        answer: "Au"
+    },
+    {
+        id: 8,
+        question: "Which planet is known as the Red Planet?",
+        answer: "Mars"
+    },
+    {
+        id: 9,
+        question: "Who painted the Mona Lisa?",
+        answer: "Leonardo da Vinci"
+    },
+    {
+        id: 10,
+        question: "What is the largest ocean on Earth?",
+        answer: "Pacific Ocean"
+    }
+];
+
+
+export default data;
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    // console.log(getCurrentId);
+    setSelected(getCurrentId);
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+                {selected === dataItem.id ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : null}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>NO data found !!</div>
+        )}
+      </div>
+    </div>
+  );
+}
